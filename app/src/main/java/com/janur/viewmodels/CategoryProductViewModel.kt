eece3439677d6427fdedd040540fd6a6500c9111
User <user@192.168.1.3>
2023-12-10 15:21:10 +0700

    Adding Product Category

--- app/src/main/java/com/janur/viewmodels/CategoryProductViewModel.kt
+++ app/src/main/java/com/janur/viewmodels/CategoryProductViewModel.kt
@@ -13,8 +13,22 @@ class CategoryProductViewModel : ViewModel() {
     init {
         // Isi kategori dari sumber data
         val categoriesList = listOf(
-            CategoryProductModel(R.drawable.category_product_icon, "Category 1"),
-            CategoryProductModel(R.drawable.category_product_icon, "Category 2"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Dekorasi"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Wedding Organizer"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Makeup"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Photo Video"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Catering"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Entertain"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Category 7"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Category 8"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Category 9"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Category 10"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Category 11"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Category 12"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Category 13"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Category 14"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Category 15"),
+            CategoryProductModel(R.drawable.baseline_android_24, "Category 16"),
             // ...tambahkan kategori lainnya
         )
         categories.value = categoriesList
--- app/src/main/java/com/janur/views/CategoryProductFragment.kt
+++ app/src/main/java/com/janur/views/CategoryProductFragment.kt
@@ -1,11 +1,15 @@
 package com.janur.views
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.janur.R
@@ -15,8 +19,6 @@ import com.janur.viewmodels.CategoryProductViewModel
 class CategoryProductFragment : Fragment() {
 
     private lateinit var categoryViewModel: CategoryProductViewModel
-    private lateinit var categoryAdapter: CategoryProductAdapter // Buat adapter sesuai kebutuhanmu
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +34,13 @@ class CategoryProductFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_category_product, container, false)
 
         // Inisialisasi RecyclerView
-        val recyclerView: RecyclerView = root.findViewById(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        categoryAdapter = CategoryProductAdapter() // Sesuaikan dengan adapter yang kamu buat
+        val recyclerView: RecyclerView = root.findViewById(R.id.rvProductCategory)
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+
+        recyclerView.setHasFixedSize(false)
+
+        //set adapter
+        val categoryAdapter = CategoryProductAdapter() // Sesuaikan dengan adapter yang kamu buat
         recyclerView.adapter = categoryAdapter
 
         // Inisialisasi ViewModel
@@ -43,10 +49,15 @@ class CategoryProductFragment : Fragment() {
             // Update data ke adapter saat data berubah
             categoryAdapter.submitList(categories)
         })
-
         return root
     }
 
-    companion object {}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+//        toolbar
+//        val toolbar = requireView().findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbarProductCategory)
+//        toolbar.title = "Kategori Produk"
+//        (requireActivity() as AppCompatActivity).setSupportActionBar(null)
+    }
 }
