7ed6dbe58b32d74c3791d06b4b805c23e6ea156a
User <user@192.168.1.3>
2023-12-12 14:48:31 +0700

    Adding Detail Vendor

---
+++ app/src/main/java/com/janur/DetailVendorActivity.kt
@@ -0,0 +1,11 @@
+package com.janur
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailVendorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_vendor)
+    }
+}
